8b89d259f1d613c79d07b2b09d5f3592bbbbd117
localseo365 <97588881+localseo365@users.noreply.github.com>
2022-07-05 12:54:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+error_reporting(0);
 require_once "config/config.php";
 //require_once "libs/database.php";
 require_once "libs/controller.php";
 require_once "libs/view.php";
 //require_once "libs/model.php";
 require_once "libs/app.php";
-require_once "classes/sitemapgen.php";
-sitemapGenerate();
+//require_once "classes/sitemapgen.php";
+//sitemapGenerate();
 $app = new App;
 ?>
